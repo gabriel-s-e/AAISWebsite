bd3420fe6d1179228a3c9911c08153c6747cbc2a
Isaac <isaacfunes@icloud.com>
2024-02-16 18:24:28 -0600

    Started layout

---
+++ src/app/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar, Footer } from '../components'
+
+const Home = () => {
+  return (
+    <>
+        <Navbar />
+        <p>dffsdf</p>
+        <Footer />
+    </>
+  )
+}
+
+export default Home
