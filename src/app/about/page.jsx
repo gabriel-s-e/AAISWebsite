e42c4a6c00300120e1a37bfefbf663046fc54ca9
hannaharmon <hannamarieharmon@gmail.com>
2024-02-18 17:08:15 -0600

    Fixed page structure

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
+export default function About() {
+  return (
+    <div>
+      <Navbar />
+      <h1>about</h1>
+      <Footer />
+    </div>
+  );
+}
