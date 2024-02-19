f1c726f27f9744d74f7867dd1a1fe3ded17a9fd3
hannaharmon <hannamarieharmon@gmail.com>
2024-02-19 08:03:59 -0600

    Added navbar icons (placeholder)

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,7 +1,7 @@
 import styles from "./Bar.module.css"
 import { FaDiscord, FaLinkedin, FaInstagram } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
-import { Link } from "react-router-dom";
+import Link from "next/link"
 
 export default function Footer() {
   return (
@@ -9,15 +9,15 @@ export default function Footer() {
       <div className={styles.links}>
         <div className={styles.linkbox}>
           <div className={styles.links}>
-            <a href="https://www.instagram.com/" target="_blank" className={styles.marginshape}> <FaInstagram size={60}/> </a>
-            <a href="https://discord.com/" target="_blank" className={styles.marginshape}> <FaDiscord size={60}/> </a>
-            <a href="https://www.linkedin.com/" target="_blank" className={styles.marginshape}> <FaLinkedin size={60}/> </a>
-            <a href="mailto:" target="_blank" className={styles.marginshape}> <SiGmail size={60}/> </a>
+            <a href="https://www.instagram.com/" target="_blank" className={styles.marginshape}> <FaInstagram size={55}/> </a>
+            <a href="https://discord.com/" target="_blank" className={styles.marginshape}> <FaDiscord size={55}/> </a>
+            <a href="https://www.linkedin.com/" target="_blank" className={styles.marginshape}> <FaLinkedin size={55}/> </a>
+            <a href="mailto:" target="_blank" className={styles.marginshape}> <SiGmail size={55}/> </a>
           </div>
         </div>
-        <div className={styles.meetbox}>
+        <Link href="/events" className={styles.meetbox}>
           Next meeting: Eventually
-        </div>
+        </Link>
       </div>
     </div>
   )
--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,5 +1,7 @@
 import Link from "next/link"
 import styles from "./Bar.module.css"
+import { PiHexagonBold } from "react-icons/pi";
+import { MdOutlineCalendarToday, MdOutlinePerson, MdOutlineQuiz, MdChatBubbleOutline, MdBookmarkBorder  } from "react-icons/md";
 
 export default function Navbar() {
   return (
@@ -7,27 +9,27 @@ export default function Navbar() {
       {/* <div className={styles.navbartop}></div> */}
       <div className={styles.links}>
         <Link href="/" className={styles.button}>
-          <div className={styles.shape}></div>
+          <div className={styles.shape}><PiHexagonBold size={50}/></div>
           <div className={styles.buttontitle}>Home</div>
         </Link>
         <Link href="/about" className={styles.button}>
-          <div className={styles.shape}></div>
+          <div className={styles.shape}><MdOutlinePerson size={50}/></div>
           <div className={styles.buttontitle}>About</div>
         </Link>
         <Link href="/events" className={styles.button}>
-          <div className={styles.shape}></div>
+          <div className={styles.shape}><MdOutlineCalendarToday size={48}/></div>
           <div className={styles.buttontitle}>Events</div>
         </Link>
         <Link href="/resources" className={styles.button}>
-          <div className={styles.shape}></div>
+          <div className={styles.shape}><MdBookmarkBorder size={50}/></div>
           <div className={styles.buttontitle}>Resources</div>
         </Link>
         <Link href="/FAQ" className={styles.button}>
-          <div className={styles.shape}></div>
+          <div className={styles.shape}><MdOutlineQuiz size={50}/></div>
           <div className={styles.buttontitle}>FAQs</div>
         </Link>
         <Link href="/contact" className={styles.button}>
-          <div className={styles.shape}></div>
+          <div className={styles.shape}><MdChatBubbleOutline size={50}/></div>
           <div className={styles.buttontitle}>Contact</div>
         </Link>
       </div>
