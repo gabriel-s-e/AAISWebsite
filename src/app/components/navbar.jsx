baf2fb3ae79027fedc84894af7d46711d25e578c
hannaharmon <hannamarieharmon@gmail.com>
2024-02-19 05:43:00 -0600

    Added rough navbar component styling and font

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,14 +1,36 @@
 import Link from "next/link"
+import styles from "./Navbar.module.css"
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/events">Events</Link>
-      <Link href="/resources">Resources</Link>
-      <Link href="/FAQ">FAQs</Link>
-      <Link href="/contact">Contact</Link>
+    <nav className={styles.navbar}>
+      <div className={styles.navbartop}></div>
+      <div className={styles.links}>
+        <Link href="/" className={styles.button}>
+          <div className={styles.shape}></div>
+          <div className={styles.buttontitle}>Home</div>
+        </Link>
+        <Link href="/about" className={styles.button}>
+          <div className={styles.shape}></div>
+          <div className={styles.buttontitle}>About</div>
+        </Link>
+        <Link href="/events" className={styles.button}>
+          <div className={styles.shape}></div>
+          <div className={styles.buttontitle}>Events</div>
+        </Link>
+        <Link href="/resources" className={styles.button}>
+          <div className={styles.shape}></div>
+          <div className={styles.buttontitle}>Resources</div>
+        </Link>
+        <Link href="/FAQ" className={styles.button}>
+          <div className={styles.shape}></div>
+          <div className={styles.buttontitle}>FAQs</div>
+        </Link>
+        <Link href="/contact" className={styles.button}>
+          <div className={styles.shape}></div>
+          <div className={styles.buttontitle}>Contact</div>
+        </Link>
+      </div>
     </nav>
   )
 }
