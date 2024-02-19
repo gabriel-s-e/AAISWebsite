eee43d798d2d75109293de23e61197db6201febf
hannaharmon <hannamarieharmon@gmail.com>
2024-02-19 07:24:16 -0600

    Added footer icons and placeholder links

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,4 +1,7 @@
 import styles from "./Bar.module.css"
+import { FaDiscord, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
@@ -6,10 +9,10 @@ export default function Footer() {
       <div className={styles.links}>
         <div className={styles.linkbox}>
           <div className={styles.links}>
-            <div className={styles.marginshape}> </div>
-            <div className={styles.marginshape}> </div>
-            <div className={styles.marginshape}> </div>
-            <div className={styles.marginshape}> </div>
+            <a href="https://www.instagram.com/" target="_blank" className={styles.marginshape}> <FaInstagram size={60}/> </a>
+            <a href="https://discord.com/" target="_blank" className={styles.marginshape}> <FaDiscord size={60}/> </a>
+            <a href="https://www.linkedin.com/" target="_blank" className={styles.marginshape}> <FaLinkedin size={60}/> </a>
+            <a href="mailto:" target="_blank" className={styles.marginshape}> <SiGmail size={60}/> </a>
           </div>
         </div>
         <div className={styles.meetbox}>
